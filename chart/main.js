79bc99efbe6374a619b464329ec716f67b30627b
Joao Meidanis <joao@DESKTOP-TSE4B4J>
2020-05-29 18:51:38 -0300

    image sizes, bubble graph

--- chart/main.js
+++ chart/main.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-    var chartCtx = document.getElementById('forChart').getContext('2d');
+    var chartCtx = document.getElementById('color').getContext('2d');
     chart = new Chart(chartCtx, {
 	// The type of chart we want to create
 	type: 'bar',
@@ -10,27 +10,86 @@ window.onload = function() {
 	    labels: ['Gray scale', 'Colored'],
 	    datasets: [
 		{
-		    label: 'Train',
-		    backgroundColor: 'rgb(255, 99, 132)',
-		    borderColor: 'rgb(255, 99, 132)',
-		    barThickness: 'flex',
-		    barPercentage: 1.0,
-		    data: [8270, 17091]
-		},
-		{
 		    label: 'Test',
 		    backgroundColor: 'rgb(34, 145, 32)',
 		    borderColor: 'rgb(120, 45, 149)',
 		    barThickness: 'flex',
 		    barPercentage: 1.0,
 		    data: [2089, 5871]
-		}]
+		}
+		,
+		{
+		    label: 'Train',
+		    backgroundColor: 'rgb(255, 99, 132)',
+		    borderColor: 'rgb(255, 99, 132)',
+		    barThickness: 'flex',
+		    barPercentage: 1.0,
+		    data: [8270, 17091]
+		}
+	    ]
 	},
 	
 	// Configuration options go here
 	options: {
 	    scales: {
 		yAxes: [{
+		    ticks: {
+			min: 0
+		    }
+		}]
+	    }
+	}
+    });
+
+    var chartCtx = document.getElementById('size').getContext('2d');
+    chart = new Chart(chartCtx, {
+	// The type of chart we want to create
+	type: 'bubble',
+	label: "Image Size",
+	// The data for our dataset
+	data: {
+	    datasets: [
+	    	{
+		    label: 'Test',
+		    backgroundColor: 'rgb(34, 145, 32)',
+		    borderColor: 'rgb(120, 45, 149)',
+		    barThickness: 'flex',
+		    barPercentage: 1.0,
+		    data: [
+			{x: 1050, y: 700, r: 7},
+			{x: 1050, y: 600, r: 3},
+			{x: 1050, y: 525, r: 2},
+			{x:  700, y: 500, r: 1},
+			{x: 1050, y: 450, r: 3}			
+		    ]
+		}
+		,
+		{
+		    label: 'Train',
+		    backgroundColor: 'rgb(255, 99, 132)',
+		    borderColor: 'rgb(255, 99, 132)',
+		    barThickness: 'flex',
+		    barPercentage: 1.0,
+		    data: [
+			{x: 1050, y: 700, r: 18},
+			{x: 1050, y: 600, r: 14},
+			{x: 1050, y: 525, r: 7},
+			{x:  700, y: 500, r: 3},
+			{x: 1050, y: 450, r: 8}
+		    ]
+		}
+	    ]
+	},
+	
+	// Configuration options go here
+	options: {
+	    scales: {
+		yAxes: [{
+		    ticks: {
+			min: 0
+		    }
+		}],
+		xAxes: [{
 		    ticks: {
 			min: 0
 		    }
