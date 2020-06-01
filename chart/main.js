04a31461880f553d156e533ce6702fb99f7ebd18
Henrique Simões <henrique.simoes@students.ic.unicamp.br>
2020-06-01 12:53:10 -0300

    [Chart]: Add real data plotting
    It is possible to use a dummy data for quick loading and rendering.

--- chart/main.js
+++ chart/main.js
@@ -1,4 +1,15 @@
-function buildCharts(data) {
+/**
+ * Flag value to set whether dummy data should be used.
+ * 
+ * Note that using the real data drastically increases the loading
+ * time. Moreover, the dummy data is loaded and plotted while the
+ * real data is loading asynchronously.
+ */
+window.useDummyData = false;
+
+function createCharts() {
+	let data = window.data == undefined || window.useDummyData ? window.dummy : window.data;
+
 	var chartCtx = document.getElementById('color').getContext('2d');
 	chart = new Chart(chartCtx, {
 		// The type of chart we want to create
@@ -13,14 +24,14 @@ function buildCharts(data) {
 				borderColor: 'rgb(120, 45, 149)',
 				barThickness: 'flex',
 				barPercentage: 1.0,
-				data: [2089, 5871]
+				data: data.color.test,
 			}, {
 				label: 'Train',
 				backgroundColor: 'rgb(255, 99, 132)',
 				borderColor: 'rgb(255, 99, 132)',
 				barThickness: 'flex',
 				barPercentage: 1.0,
-				data: [8270, 17091]
+				data: data.color.train,
 			}]
 		},
 		
@@ -47,26 +58,14 @@ function buildCharts(data) {
 				borderColor: 'rgb(120, 45, 149)',
 				barThickness: 'flex',
 				barPercentage: 1.0,
-				data: [
-					{x: 1050, y: 700, r: 7},
-					{x: 1050, y: 600, r: 3},
-					{x: 1050, y: 525, r: 2},
-					{x:  700, y: 500, r: 1},
-					{x: 1050, y: 450, r: 3}			
-				]
+				data: data.sizes.test,
 			}, {
 				label: 'Train',
 				backgroundColor: 'rgb(255, 99, 132)',
 				borderColor: 'rgb(255, 99, 132)',
 				barThickness: 'flex',
 				barPercentage: 1.0,
-				data: [
-					{x: 1050, y: 700, r: 18},
-					{x: 1050, y: 600, r: 14},
-					{x: 1050, y: 525, r: 7},
-					{x:  700, y: 500, r: 3},
-					{x: 1050, y: 450, r: 8}
-				]
+				data: data.sizes.train,
 			}]
 		},
 		
@@ -92,21 +91,73 @@ function buildCharts(data) {
 	});
 };
 
-function processClasses(data) {
+function processClasses(csv) {
 
 }
 
-function processColor(data) {
+function processColor(csv) {
+	let parsed = Papa.parse(csv[0], { header: true });
 
+	let result = {
+		test: [0, 0],
+		train: [0, 0],
+	};
+
+	console.log("Parsing colors...");
+
+	for (row of parsed.data) {
+		result[row.Dataset][row.Colored === 'True' ? 1 : 0] = parseInt(row.Frequency);
+	}
+
+	return result;
 }
 
-function processSizes(data) {
+function processSizes(csv) {
+	const result = {};
+	const datasetNames = ['train', 'test'];
 
+	console.log("Parsing image sizes...");
+
+	for (var i = 0; i < csv.length; i++) {
+		let parsed = Papa.parse(csv[i], { header: true });
+
+		result[datasetNames[i]] = []
+
+		var maxFreq = 0;
+
+		for (row of parsed.data) {
+			let freq = parseInt(row.Frequency);
+			if (!freq) continue;
+
+			if (freq > 100) continue; 
+		
+			maxFreq = maxFreq < freq ? freq : maxFreq;
+
+			result[datasetNames[i]].push({
+				x: parseInt(row.Width),
+				y: parseInt(row.Height),
+				r: 2 // parseInt(row.Frequency),
+			});
+		}
+
+		/**
+		 * Code for rescaling proportionally to the maximum value using the rescale factor.
+		 */
+		// const rescaleFactor = 15;
+		// for (row of result[datasetNames[i]])
+		// 	row.r = parseInt(row.r * rescaleFactor / maxFreq);
+	}
+
+	return result;
 }
 
-async function loadData() {
-	alert("Loading data... this may take awhile.");
-
+/**
+ * Asynchronously loads data from the CSV files and triggers the `callback`
+ * 	when it finishes.
+ *
+ * @param {Function} callback 
+ */
+async function loadData(callback) {
 	const data = {
 		classes: {
 			filenames: ['classes.csv'],
@@ -120,26 +171,79 @@ async function loadData() {
 			filenames: ['tuple.train.csv', 'tuple.test.csv'],
 			process: processSizes,
 		},
+		loaded: false,
 	};
+
+	window.data = data;
 
 	for (type in data) {
 		let responses = [];
+		if (!data[type].filenames) continue;
+
 		for (file of data[type].filenames) {
+			console.log("Loading " + file + "...");
 			const response = await $.ajax(file).promise();
+			
+			data.loaded = true;
 			
 			responses.push(response);
 		}
 
-		data[type].data = data[type].process(responses);
+		window.data[type] = data[type].process(responses);
 	}
 
-	return data;
+	if (data.loaded)
+		callback();
+}
+
+function createDummyData() {
+	return {
+		classes: {
+			train: [],
+		},
+		color: {
+			test: [2089, 5871],
+			train: [8270, 17091],
+		},
+		sizes: {
+			test: [
+				{x: 1050, y: 700, r: 7},
+				{x: 1050, y: 600, r: 3},
+				{x: 1050, y: 525, r: 2},
+				{x:  700, y: 500, r: 1},
+				{x: 1050, y: 450, r: 3}			
+			],
+			train: [
+				{x: 1050, y: 700, r: 18},
+				{x: 1050, y: 600, r: 14},
+				{x: 1050, y: 525, r: 7},
+				{x:  700, y: 500, r: 3},
+				{x: 1050, y: 450, r: 8}
+			],
+		},
+	};
+}
+
+function updateCharts() {
+	if (!window.dummy)
+		window.dummy = createDummyData();
+
+	if (!window.data && !window.useDummyData)
+		loadData(createCharts);
+
+	// Create charts
+	createCharts();
 }
 
 function build() {
-	let data = loadData();
+	let dummyCheckbox = document.getElementById('dummyCheckbox');
+	window.useDummyData = dummyCheckbox.checked;
+	dummyCheckbox.onchange = (event) => {
+		window.useDummyData = event.target.checked;
+		updateCharts();
+	}
 
-	buildCharts(data);
+	updateCharts();
 }
 
 window.onload = build;
