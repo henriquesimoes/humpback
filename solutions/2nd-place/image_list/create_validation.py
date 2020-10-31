90ac7ba6381d6b9826382a72ba9b9937ef1bdd41
Henrique Simões <henrique.simoes@students.ic.unicamp.br>
2020-10-31 11:07:25 -0300

    Update README
    Add information about the 2nd solution in the branches description.
    In addition, the create validation script has been reformatted.

--- solutions/2nd-place/image_list/create_validation.py
+++ solutions/2nd-place/image_list/create_validation.py
@@ -1,32 +1,36 @@
-import pandas as pd
 import argparse
 
+import pandas as pd
+
+
 def get_config():
-  ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser()
 
-  ap.add_argument('--labels', type=str, default='label_list.txt', help='Label mapping to integers')
-  ap.add_argument('--train', type=str, required=True, help='Train set labels')
-  ap.add_argument('--test_number', type=int, required=True, help="Test number for the output name")
-  ap.add_argument('--percent', type=float, default=0.02365, help="set percentage to use as validation")
+    ap.add_argument('--labels', type=str, default='label_list.txt', help='label mapping to integers')
+    ap.add_argument('--train', type=str, required=True, help='training set labels')
+    ap.add_argument('--test_number', type=int, required=True, help="test number of the output file name")
+    ap.add_argument('--percent', type=float, default=0.02365, help="set percentage to use as validation")
 
-  return ap.parse_args()
+    return ap.parse_args()
+
 
 def main():
-  config = get_config()
+    config = get_config()
 
-  df_label = pd.read_csv(config.labels, sep=' ', names=['Id', 'Number']).set_index('Id')
-  df = pd.read_csv(config.train).set_index('Image')
+    df_label = pd.read_csv(config.labels, sep=' ', names=['Id', 'Number']).set_index('Id')
+    df = pd.read_csv(config.train).set_index('Image')
 
-  for id, num in df_label.iterrows():
-    df.loc[df['Id'] == id, 'Id'] = df_label.loc[id]['Number']
+    for id, num in df_label.iterrows():
+        df.loc[df['Id'] == id, 'Id'] = df_label.loc[id]['Number']
 
-  df_val = df.sample(frac=config.percent)
+    df_val = df.sample(frac=config.percent)
 
-  print(len(df_val))
-  print(len(df_val[df_val['Id'] == -1]) / len(df_val))
+    print(len(df_val))
+    print(len(df_val[df_val['Id'] == -1]) / len(df_val))
 
-  df_val.to_csv(f'test{config.test_number}.val.txt', sep=' ', header=False)
-  df.to_csv(f'test{config.test_number}.train.txt', sep=' ', header=False)
+    df_val.to_csv(f'test{config.test_number}.val.txt', sep=' ', header=False)
+    df.to_csv(f'test{config.test_number}.train.txt', sep=' ', header=False)
+
 
 if __name__ == "__main__":
-  main()
+    main()
